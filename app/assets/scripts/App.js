45f362c5273c9273587e643a4f6643daa33d1e09
Ijal Fauzi <ijaliffauzi@gmail.com>
2022-06-24 16:20:57 +0700

    Added reveal on scroll function.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,12 @@
 import '../styles/styles.css';
 import Navigation from './modules/Navigation'
+import RevealOnScroll from './modules/RevealOnScroll'
 
+
+new RevealOnScroll(document.querySelectorAll(".service"), 75)
+new RevealOnScroll(document.querySelectorAll(".post"), 75)
+new RevealOnScroll(document.querySelectorAll(".partner"), 75)
+new RevealOnScroll(document.querySelectorAll(".widget-content"), 75)
 
 if (module.hot) {
     module.hot.accept()

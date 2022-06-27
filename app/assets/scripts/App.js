f570d3a739f732ed6a2662e7925d3ee8c93b2a08
Ijal Fauzi <ijaliffauzi@gmail.com>
2022-06-27 15:14:45 +0700

    Removed GA & GTM from bundled.js.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,6 @@
 import '../styles/styles.css';
 import Navigation from './modules/Navigation'
 import RevealOnScroll from './modules/RevealOnScroll'
-import GoogleAnalytics from './modules/GoogleAnalytics'
-import GoogleTagManagr from './modules/GoogleTagManager'
-
 
 new RevealOnScroll(document.querySelectorAll(".service"), 75)
 new RevealOnScroll(document.querySelectorAll(".post"), 75)

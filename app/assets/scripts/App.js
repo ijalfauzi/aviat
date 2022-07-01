b569d95c5364825e8589b1bc22237c29f6fc8b9d
Ijal Fauzi <ijaliffauzi@gmail.com>
2022-07-01 22:04:42 +0700

    Splitted modal search with webpack.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,9 +6,22 @@ new RevealOnScroll(document.querySelectorAll(".service"), 75)
 new RevealOnScroll(document.querySelectorAll(".post"), 75)
 new RevealOnScroll(document.querySelectorAll(".partner"), 75)
 new RevealOnScroll(document.querySelectorAll(".widget-content"), 75)
-import Modal from './modules/Modal'
 
-new Modal()
+let modal 
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept()

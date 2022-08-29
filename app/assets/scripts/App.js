f6daca2ecf8b9664b0a5e379b3b162000d109936
Ijal Fauzi <ijaliffauzi@gmail.com>
2022-08-29 11:02:47 +0700

    Updated modal with vanilla JS.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,22 +7,6 @@ new RevealOnScroll(document.querySelectorAll(".post"), 75)
 new RevealOnScroll(document.querySelectorAll(".partner"), 75)
 new RevealOnScroll(document.querySelectorAll(".widget-content"), 75)
 
-let modal 
-
-document.querySelectorAll(".open-modal").forEach(el => {
-    el.addEventListener("click", e => {
-        e.preventDefault()
-        if (typeof modal == "undefined") {
-            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(() => modal.openTheModal(), 20)
-            }).catch(() => console.log("There was a problem."))
-        } else {
-            modal.openTheModal()
-        }
-    })
-})
-
 if (module.hot) {
     module.hot.accept()
 }
